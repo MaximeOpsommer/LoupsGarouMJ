62387953641e3baf4aabeb362bd16176e242115d
Maxime Opsommer <maxime.opsommer@gmail.com>
2018-12-18 22:12:46 +0100

    first commit

---
+++ app/src/main/java/maximeopsommer/android/loupsgaroumj/GameCreationActivity.java
@@ -0,0 +1,67 @@
+package maximeopsommer.android.loupsgaroumj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameCreationActivity extends AppCompatActivity {
+
+    private EditText newPlayerName;
+    private Button addPlayerButton;
+    private ListView playersList;
+    private Button cancelButton;
+
+    private final List<String> playersName = new ArrayList<>();
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_game);
+        newPlayerName = findViewById(R.id.newPlayerName);
+        addPlayerButton = findViewById(R.id.addPlayerButton);
+        addPlayerButton.setOnClickListener(e -> checkPlayerName());
+        playersList = findViewById(R.id.playersList);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, playersName);
+        cancelButton = findViewById(R.id.cancelButton);
+        playersList.setAdapter(adapter);
+        cancelButton.setOnClickListener(e -> switchToMainView());
+    }
+
+    private void checkPlayerName() {
+        final String newPlayerNameText = newPlayerName.getText().toString();
+        if (!newPlayerNameText.isEmpty()) {
+            if (playersName.contains(newPlayerNameText)) {
+                new AlertDialog.Builder(this)
+                        .setMessage("There is already a player with that name.")
+                        .setTitle("ERROR")
+                        .create()
+                        .show();
+            } else {
+                addPlayer(newPlayerNameText);
+                newPlayerName.setText("");
+            }
+        }
+
+    }
+
+    private void addPlayer(final String playerName) {
+        playersName.add(playerName);
+    }
+
+    private void switchToMainView() {
+        final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+
+}
---
+++ app/src/main/java/maximeopsommer/android/loupsgaroumj/MainActivity.java
@@ -0,0 +1,43 @@
+package maximeopsommer.android.loupsgaroumj;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mainTitle();
+        final Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(e -> switchToCreateGameView());
+    }
+
+    private void mainTitle() {
+        final AssetManager assetManager = getApplicationContext().getAssets();
+        final TextView textView = findViewById(R.id.mainTitle);
+        final Typeface typeface = Typeface.createFromAsset(assetManager, "fonts/GypsyCurse.ttf");
+        textView.setTypeface(typeface);
+        final Display display = getWindowManager().getDefaultDisplay();
+        final Point size = new Point();
+        display.getSize(size);
+        final int screenWidth = size.x;
+        final int screenHeight = size.y;
+        textView.setHeight((int) (screenHeight * 0.15));
+    }
+
+    private void switchToCreateGameView() {
+        final Intent intent = new Intent(getApplicationContext(), GameCreationActivity.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+}
